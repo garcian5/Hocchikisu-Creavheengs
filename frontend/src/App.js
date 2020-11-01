2ab915b889c30d25ce2ad26126a23e8948d360a8
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-31 21:13:30 -0600

    added test navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Footer from './components/presentational/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './components/presentational/NavBar';
+
 
 function App() {
     return (
         <div className="App">
+            <NavBar />
             <div className="container">
                 <p>Bootstrap Test</p>
             </div>
+            <p>Body</p>
             <Footer />
         </div>
     );
---
+++ frontend/src/components/presentational/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="center">
+                <p>link</p>
+            </div>
+            <a class="navbar-brand" href="https://getbootstrap.com/docs/4.0/components/navbar/">HocchiCreav</a>
+            <div className="center">
+                <p>link</p>
+            </div>
+        </nav>
+    )
+}
+
+// bootstrap centering
+// https://www.codeply.com/go/kTGlK9Axdk
+export default NavBar;
