db9bb030f38aa423ffdf76698efbca27580436a0
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-31 20:38:33 -0600

    added bootstrap dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Footer from './components/presentational/Footer';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
     return (
         <div className="App">
+            <div className="container">
+                <p>Bootstrap Test</p>
+            </div>
             <Footer />
         </div>
     );
--- frontend/src/components/presentational/Footer.js
+++ frontend/src/components/presentational/Footer.js
@@ -4,21 +4,23 @@ function Footer() {
     return (
         <footer>
             <div className="row">
-                <ul className="social-links">
-                    {/*target = "_blank" will open link to new tab*/}
-                    <li><a href="https://github.com/garcian5/ToDoList" rel="noopener noreferrer" target="_blank" title="Nheljee's Github"><ion-icon name="logo-github"></ion-icon></a></li>
-                    <li><a href="https://www.linkedin.com/in/nheljee-rose-garcia/" rel="noopener noreferrer" target="_blank" title="Nheljee's LinkedIn"><ion-icon name="logo-linkedin"></ion-icon></a></li>
-                    <li><a href="https://nheljee-dice-game.netlify.app/" rel="noopener noreferrer" target="_blank" title="Other Websites"><ion-icon name="apps-outline"></ion-icon></a></li>
-                </ul>
-            </div>
-            <div className="row">                
-                <p>
-                    Created by Nheljee Garcia.
-                </p>
-                <div className="credits">
-                    Icons made by <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai </a>
-                    from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                <div className="col">
+                    <ul className="social-links">
+                        {/*target = "_blank" will open link to new tab*/}
+                        <li><a href="https://github.com/garcian5/ToDoList" rel="noopener noreferrer" target="_blank" title="Nheljee's Github"><ion-icon name="logo-github"></ion-icon></a></li>
+                        <li><a href="https://www.linkedin.com/in/nheljee-rose-garcia/" rel="noopener noreferrer" target="_blank" title="Nheljee's LinkedIn"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                        <li><a href="https://nheljee-dice-game.netlify.app/" rel="noopener noreferrer" target="_blank" title="Other Websites"><ion-icon name="apps-outline"></ion-icon></a></li>
+                    </ul>
                 </div>
+            </div>
+            <div className="row">
+                <p className="author">
+                    Created by Nheljee Garcia.
+                </p>                
+            </div>
+            <div className="credits">
+                Favicon made by <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai </a>
+                from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
             </div>
         </footer>        
     )
