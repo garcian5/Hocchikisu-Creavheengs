2249ca039bb5ed8c34f1a90974aee92b7b426fb9
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-11-02 20:36:17 -0700

    updated CreateAccount and styles

--- frontend/src/components/presentational/user/CreateAccount.js
+++ frontend/src/components/presentational/user/CreateAccount.js
@@ -2,14 +2,31 @@ import React from "react";
 
 function CreateAccount() {
 	return (
-		<div className="login-create">
-			<div className="container logcrea-box">
-				<h1 className="center logcrea-greeting">Welcome to HocchiCreav!</h1>
-				<div className="center">
-					<button className="logcrea-btn light-blue">Let's Get You Started!</button>
-					<p className="or">--OR--</p>
-					<button className="logcrea-btn light-grey">Log In</button>
-				</div>
+		<div className="create-acct">
+			<div className="container create-acct-box">
+				<h1 className="center create-acct-greeting">Welcome to HocchiCreav!</h1>
+				<h6 className="center" style={{ marginBottom: 35 }}>Sign Up!</h6>
+
+				<form className="center">
+					<div className="signup-box">
+						<input className="names" type="text" placeholder="First Name" />
+						<input className="names" type="text" placeholder="Last Name" />
+					</div>
+
+					<div className="signup-box">
+						<input type="email" placeholder="Email" />
+					</div>
+
+					<div className="signup-box">
+						<input type="password" placeholder="Password" />
+					</div>
+
+					<div className="signup-box">
+						<input type="date" placeholder="Birthday" />
+					</div>
+
+					<button className="create-acct-btn light-blue">Create Account</button>
+				</form>
 			</div>
 		</div>
 	)
