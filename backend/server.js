88622e261b5b501a681bb2e137264c91edd7f339
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-11-08 19:14:34 -0700

    added mongodb connection to server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,24 @@ const cors = require('cors');
 // use this later for mongodb connection
 const mongoose = require('mongoose');
 
+// configure environment variables
+require('dotenv').config();
+
 // initialize express server
 const app = express();
 
 // add cors and express middlewares
 app.use(cors());
 app.use(express.json());
+
+const mongodb_uri = process.env.MONGODB_URI;
+
+// connect to mongodb w/ mongoose
+mongoose.connect(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Successfully connected to MongoDB...");
+})
 
 // define server port
 const port = process.env.PORT || 5000;
