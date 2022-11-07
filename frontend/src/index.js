54abc7c21ecbf50e3b6a460f4a2bffa2de23fd94
Nheljee Rose Garcia <ngarcia@launch57.com>
2022-11-07 00:26:02 -0700

    upgrading react version to 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// for previous react versions (17 and below)
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+// for latest react versions (18+)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
